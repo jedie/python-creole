f5a208ee56930e349efe5d8a504df74609aa7d6c
JensDiemer <git@jensdiemer.de>
2011-10-17 10:45:11 +0200

    move test helpers

--- creole/tests/__init__.py
+++ creole/tests/__init__.py
@@ -11,93 +11,10 @@
 
 from __future__ import division, absolute_import, print_function, unicode_literals
 
-from doctest import testmod
-import os
-import sys
-import time
 import unittest
+from unittest.runner import TextTestRunner
 
-import creole
 from creole.tests import all_tests
-
-
-SKIP_DIRS = (".settings", ".git", "dist", "python_creole.egg-info")
-SKIP_FILES = ("setup.py", "test.py")
-
-
-if "-v" in sys.argv or "--verbosity" in sys.argv:
-    VERBOSE = 2
-elif "-q" in sys.argv or "--quite" in sys.argv:
-    VERBOSE = 0
-else:
-    VERBOSE = 1
-
-
-def run_all_doctests(verbosity=None):
-    """
-    run all python-creole DocTests
-    """
-    start_time = time.time()
-    if verbosity is None:
-        verbosity = VERBOSE
-
-    path = os.path.abspath(os.path.dirname(creole.__file__))
-    if verbosity >= 2:
-        print("")
-        print("_" * 79)
-        print("Running %r DocTests:\n" % path)
-
-    total_files = 0
-    total_doctests = 0
-    total_attempted = 0
-    total_failed = 0
-    for root, dirs, filelist in os.walk(path, followlinks=True):
-        for skip_dir in SKIP_DIRS:
-            if skip_dir in dirs:
-                dirs.remove(skip_dir) # don't visit this directories
-
-        for filename in filelist:
-            if not filename.endswith(".py"):
-                continue
-            if filename in SKIP_FILES:
-                continue
-
-            total_files += 1
-
-            sys.path.insert(0, root)
-            try:
-                m = __import__(filename[:-3])
-            except ImportError as err:
-                if verbosity >= 2:
-                    print("***DocTest import %s error*** %s" % (filename, err))
-            except Exception as err:
-                if verbosity >= 2:
-                    print("***DocTest %s error*** %s" % (filename, err))
-            else:
-                failed, attempted = testmod(m)
-                total_attempted += attempted
-                total_failed += failed
-                if attempted or failed:
-                    total_doctests += 1
-
-                if attempted and not failed:
-                    filepath = os.path.join(root, filename)
-                    if verbosity <= 1:
-                        sys.stdout.write(".")
-                    elif verbosity >= 2:
-                        print("DocTest in %s OK (failed=%i, attempted=%i)" % (
-                            filepath, failed, attempted
-                        ))
-            finally:
-                del sys.path[0]
-
-    duration = time.time() - start_time
-    print("")
-    print("-"*70)
-    print("Ran %i DocTests from %i files in %.3fs: failed=%i, attempted=%i\n\n" % (
-        total_doctests, total_files, duration, total_failed, total_attempted
-    ))
-
 
 
 def get_test_suite():
@@ -108,22 +25,12 @@ def get_test_suite():
     return suite
 
 
-def run_unittests(verbosity=None):
-    """
-    run all python-creole unittests
-    """
-    if verbosity is None:
-        verbosity = VERBOSE
-
-    if verbosity >= 2:
-        print("")
-        print("_" * 79)
-        print("Running Unittests:\n")
-
-    unittest.main(verbosity=verbosity)
-
-
 if __name__ == '__main__':
-    run_all_doctests()
-    run_unittests()
+    suite = get_test_suite()
+
+#    for entry in suite:
+#        print(entry)
+
+    runner = TextTestRunner()
+    runner.run(suite)
 
