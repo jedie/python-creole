e8c0c15bd21724c612ef58ee039a81d70ece2848
JensDiemer <git@jensdiemer.de>
2015-04-05 14:42:27 +0200

    release as v1.2.2

--- creole/__init__.py
+++ creole/__init__.py
@@ -20,7 +20,7 @@
 
 from __future__ import division, absolute_import, print_function, unicode_literals
 
-__version__ = (1, 2, 1)
+__version__ = (1, 2, 2)
 __api__ = (1, 0) # Creole 1.0 spec - http://wikicreole.org/
 
 
