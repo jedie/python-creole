51295d3a7251d29b6701d9c43fae83e8082614a9
JensDiemer <git@jensdiemer.de>
2020-01-18 23:26:35 +0100

    Simplify subprocess mixin

--- creole/tests/test_subprocess.py
+++ creole/tests/test_subprocess.py
@@ -4,41 +4,23 @@
     unittest
     ~~~~~~~~
 
-    :copyleft: 2015 by python-creole team, see AUTHORS for more details.
+    :copyleft: 2015-2020 by python-creole team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
 
-
-import unittest
-import sys
 import os
+import sys
+import unittest
 
 from creole.tests.utils.unittest_subprocess import SubprocessMixin
 
 
 class TestSubprocessMixin(unittest.TestCase, SubprocessMixin):
-    def test_find_executable(self):
-        filepath = self.find_executable("python")
-        if not hasattr(self, "assertRegex"): # New in version 3.1
-            self.assertRegex = self.assertRegexpMatches
-        self.assertRegex(filepath, ".*?python.*?")
-
-    def test_executable_not_exists(self):
-        with self.assertRaisesRegexp(AssertionError, """Program "doesn't exists!" not found in:.*"""):
-            self.find_executable("doesn't exists!")
-
-    def test_executable_with_path(self):
-        msg = "'%s' unexpectedly found in '%s'" % (
-            os.sep, sys.executable
-        )
-        with self.assertRaisesRegexp(AssertionError, msg):
-            self.find_executable(sys.executable)
 
     def test_subprocess(self):
         popen_args, retcode, stdout = self.subprocess(
             popen_args=[sys.executable, "-c", "import sys;sys.stdout.write('to stdout')"],
-            verbose=False
         )
         self.assertEqual(stdout, "to stdout")
         self.assertEqual(retcode, 0)
@@ -64,6 +46,4 @@ class TestSubprocessMixin(unittest.TestCase, SubprocessMixin):
                 "to stdout 2\n"
                 "to stderr 2\n"
             ),
-            verbose=True
         )
-
