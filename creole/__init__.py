bbfc21fb6b71e9c8f852d4bd2bce9bbdc586876d
JensDiemer <git@jensdiemer.de>
2020-02-01 16:47:29 +0100

    release as v1.4.2

--- creole/__init__.py
+++ creole/__init__.py
@@ -23,7 +23,7 @@ from creole.emitter.html2textile_emitter import TextileEmitter
 from creole.parser.creol2html_parser import CreoleParser
 from creole.parser.html_parser import HtmlParser
 
-__version__ = "1.4.2.dev2"
+__version__ = "1.4.2"
 __api__ = "1.0"  # Creole 1.0 spec - http://wikicreole.org/
 
 VERSION_STRING = __version__  # remove in future
